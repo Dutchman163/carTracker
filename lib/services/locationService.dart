04dca90e4af4d549dadb57012f59c4f7968b49e7
Johan Luiken <johan_luiken@hotmail.com>
2025-05-17 21:25:54 +0200

    Generated

--- lib/services/locationService.dart
+++ lib/services/locationService.dart
@@ -5,22 +5,34 @@ class LocationService {
   Position? _lastPosition;
   double _totalDistance = 0;
   StreamSubscription<Position>? _subscription;
+  
+  Future<void> startTracking(
+    Stream<Position> positionStream,
+    Function(double) onDistanceUpdated,
+  ) async {
+  _subscription = positionStream.listen((Position position) {
+    if (position.accuracy > 20) {
+      return; 
+    }
 
-  Future<void> startTracking(Stream<Position> positionStream, Function(double) onDistanceUpdated) async {
-    _subscription = positionStream.listen((Position position) {
-      if (_lastPosition != null) {
-        final distance = Geolocator.distanceBetween(
-          _lastPosition!.latitude,
-          _lastPosition!.longitude,
-          position.latitude,
-          position.longitude,
-        );
+    if (_lastPosition != null) {
+      final distance = Geolocator.distanceBetween(
+        _lastPosition!.latitude,
+        _lastPosition!.longitude,
+        position.latitude,
+        position.longitude,
+      );
+      if (distance >= 5) {
         _totalDistance += distance;
         onDistanceUpdated(_totalDistance);
+        _lastPosition = position; // Alleen bij geldige afstand updaten
       }
+    } else {
       _lastPosition = position;
-    });
-  }
+    }
+  });
+}
+
 
   void stopTracking() {
     _subscription?.cancel();
